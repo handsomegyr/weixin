535374fe3a83c10db5cd5027096b8bbfdac7bbe8
Guo yongrong <handsomegyr@126.com>
2016-02-14 16:59:26 +0800

    增加获取用户已领取卡券接口

--- lib/Weixin/CardManager/WeixinCardManager.php
+++ lib/Weixin/CardManager/WeixinCardManager.php
@@ -2356,7 +2356,9 @@ class WeixinCardManager
     {
         $params = array();
         $params['openid'] = $openid;
+        if(!empty($card_id)){
         $params['card_id'] = $card_id;
+        }
         $access_token = $this->weixin->getToken();
         $json = json_encode($params, JSON_UNESCAPED_UNICODE);
         $rst = $this->weixin->post($this->_url . 'user/getcardlist?access_token=' . $access_token, $json);
