aef5bf1f36957df19c27d10fb09dbdd184d1582c
handsomegyr <handsomegyr@126.com>
2016-06-18 16:09:09 +0800

    增加邀请绑定客服帐号接口

--- lib/Weixin/CustomServiceManager/WeixinCustomServiceManager.php
+++ lib/Weixin/CustomServiceManager/WeixinCustomServiceManager.php
@@ -126,7 +126,7 @@ class WeixinCustomServiceManager
      * }
      * ]
      * }
-     * 参数	说明
+     * 参数 说明
      * kf_account 完整客服账号，格式为：账号前缀@公众号微信号
      * kf_nick 客服昵称
      * kf_id 客服工号
@@ -181,7 +181,7 @@ class WeixinCustomServiceManager
      * }
      * ]
      * }
-     * 参数	说明
+     * 参数 说明
      * kf_account 完整客服账号，格式为：账号前缀@公众号微信号
      * status 客服在线状态 1：pc在线，2：手机在线。若pc和手机同时在线则为 1+2=3
      * kf_id 客服工号
@@ -220,7 +220,7 @@ class WeixinCustomServiceManager
      * "nickname" : “客服1”,
      * "password" : "pswmd5",
      * }
-     * 参数	是否必须	说明
+     * 参数 是否必须 说明
      * kf_account 是 完整客服账号，格式为：账号前缀@公众号微信号，账号前缀最多10个字符，必须是英文或者数字字符。如果没有公众号微信号，请前往微信公众平台设置。
      * nickname 是 客服昵称，最长6个汉字或12个英文字符
      * password 是 客服账号登录密码，格式为密码明文的32位加密MD5值
@@ -256,6 +256,56 @@ class WeixinCustomServiceManager
     }
 
     /**
+     * 邀请绑定客服帐号
+     * 新添加的客服帐号是不能直接使用的，只有客服人员用微信号绑定了客服账号后，方可登录Web客服进行操作。此接口发起一个绑定邀请到客服人员微信号，客服人员需要在微信客户端上用该微信号确认后帐号才可用。尚未绑定微信号的帐号可以进行绑定邀请操作，邀请未失效时不能对该帐号进行再次绑定微信号邀请。
+     * 调用说明
+     * http请求方式: POST
+     * https://api.weixin.qq.com/customservice/kfaccount/inviteworker?access_token=ACCESS_TOKEN
+     * POST数据示例如下：
+     * {
+     * "kf_account" : "test1@test",
+     * "invite_wx" : "test_kfwx"
+     * }
+     * 参数说明
+     * 参数 说明
+     * kf_account 完整客服帐号，格式为：帐号前缀@公众号微信号
+     * invite_wx 接收绑定邀请的客服微信号
+     * 返回说明
+     * 返回数据示例（正确时的JSON返回结果）：
+     * {
+     * "errcode" : 0,
+     * "errmsg" : "ok"
+     * }
+     * 主要返回码
+     * 返回码 说明
+     * 0 成功
+     * 65400 API不可用，即没有开通/升级到新版客服
+     * 65401 无效客服帐号
+     * 65407 邀请对象已经是本公众号客服
+     * 65408 本公众号已发送邀请给该微信号
+     * 65409 无效的微信号
+     * 65410 邀请对象绑定公众号客服数量达到上限（目前每个微信号最多可以绑定5个公众号客服帐号）
+     * 65411 该帐号已经有一个等待确认的邀请，不能重复邀请
+     * 65412 该帐号已经绑定微信号，不能进行邀请
+     */
+    public function inviteWorker($kf_account, $invite_wx)
+    {
+        $access_token = $this->weixin->getToken();
+        $params = array();
+        $params['kf_account'] = $kf_account;
+        $params['invite_wx'] = $invite_wx;
+        
+        $json = json_encode($params, JSON_UNESCAPED_UNICODE);
+        $rst = $this->weixin->post($this->_url2 . 'kfaccount/inviteworker?access_token=' . $access_token, $json);
+        
+        if (! empty($rst['errcode'])) {
+            throw new WeixinException($rst['errmsg'], $rst['errcode']);
+        } else {
+            return $rst;
+        }
+    }
+
+    /**
      * 设置客服信息
      *
      * 接口调用请求说明
@@ -270,7 +320,7 @@ class WeixinCustomServiceManager
      * "nickname" : “客服1”,
      * "password" : "pswmd5",
      * }
-     * 参数	是否必须	说明
+     * 参数 是否必须 说明
      * kf_account 是 完整客服账号，格式为：账号前缀@公众号微信号
      * nickname 是 客服昵称，最长6个汉字或12个英文字符
      * password 是 客服账号登录密码，格式为密码明文的32位加密MD5值
@@ -319,7 +369,7 @@ class WeixinCustomServiceManager
      * curl -F media=@test.jpg "https://api.weixin.qq.com/customservice/kfacount/uploadheadimg?access_token=ACCESS_TOKEN&kf_account=KFACCOUNT"
      * 参数说明
      *
-     * 参数	是否必须	说明
+     * 参数 是否必须 说明
      * kf_account 是 完整客服账号，格式为：账号前缀@公众号微信号
      * media 是 form-data中媒体文件标识，有filename、filelength、content-type等信息
      * 返回说明
@@ -360,7 +410,7 @@ class WeixinCustomServiceManager
      * https://api.weixin.qq.com/customservice/kfaccount/del?access_token=ACCESS_TOKEN&kf_account=KFACCOUNT
      * 参数说明
      *
-     * 参数	是否必须	说明
+     * 参数 是否必须 说明
      * kf_account 是 完整客服账号，格式为：账号前缀@公众号微信号
      * 返回说明
      *
